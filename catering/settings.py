4367b6f97306bd3dd1d91f930e01023f59add3e0
Alex Rodin <bomvendador@yandex.ru>
2017-08-16 19:00:53 +0300

    change database settings

--- catering/settings.py
+++ catering/settings.py
@@ -148,8 +148,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'main/static')
 
-MEDIA_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'main/static/images')
+MEDIA_URL = '/static/media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'main/static/media/images')
 
 LOGFILE_NAME = os.path.join(BASE_DIR,  'logs/catering.log')
 # Max size allowed for one file
